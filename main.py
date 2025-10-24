ad8382ad7d1f988e4156909779505c6519b7bf02
Alyander <momoroot33@gmail.com>
2025-10-24 21:24:57 +0000

    update 0.02

--- main.py
+++ main.py
@@ -10,20 +10,19 @@ def App() -> None:
     urls = data[0]
     names = data[1]
     channel_names = data[3]
+    render(urls, names, channel_names)
+    choose = input("Another video(1) or search(2) or quit(3): ")
+    if choose == "1":
+        render(urls, names, channel_names)
+    elif choose == "2":
+        App()
+    else:
+        quit()
+def render(urls,names,channel_names):
     i = 0
     for name in names:
         print(f"{i}. {name} - {channel_names[i]}")
         i+=1
     num = input("Type Number:")
     os.system(f"mpv {urls[int(num)]} >> /dev/null")
-    choose = input("Another(1) video or search(2) or quit(3): ")
-    if choose == "1":
-        i = 0
-        for name in names:
-            print(f"{i}. {name} - {channel_names[i]}")
-            i+=1
-    elif choose == "2":
-        App()
-    else:
-        quit()
 App()
