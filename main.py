a3c231d3b9984dabf292a608ba9e21a31ffd2758
Alyander <momoroot33@gmail.com>
2025-10-24 20:54:29 +0000

    update 0.01

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from logic.logic import Logic
 import os
-def App() -> None:
-    print("""
+print("""
     Welcome to Youtube CLI!!!
     """)
+def App() -> None:
     logic = Logic("https://inv.nadeko.net")
     query = input("Search: ")
     data = logic.query(query, 1)
@@ -16,4 +16,14 @@ def App() -> None:
         i+=1
     num = input("Type Number:")
     os.system(f"mpv {urls[int(num)]} >> /dev/null")
+    choose = input("Another(1) video or search(2) or quit(3): ")
+    if choose == "1":
+        i = 0
+        for name in names:
+            print(f"{i}. {name} - {channel_names[i]}")
+            i+=1
+    elif choose == "2":
+        App()
+    else:
+        quit()
 App()
